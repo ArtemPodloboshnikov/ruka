b64b61dc02410429d717c760fd351b5f6b726611
ArtemPodloboshnikov <ArtemPS777@yandex.ru>
2024-10-12 10:35:52 +0300

    Update page.tsx

--- app/news/[title]/page.tsx
+++ app/news/[title]/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic"
 import { staticTitles } from "@/constants/system";
-import { ComponentType, Suspense } from "react";
+import { Suspense } from "react";
 
 type Params = {
     title: string;
@@ -10,26 +10,13 @@ export async function generateStaticParams(): Promise<Params[]> {
     return staticTitles
 }
 
-export async function getStaticProps(context: { params: { title: string } }) {
-    const title = context.params.title
-    const Content = dynamic(()=>import(`../../../news/${decodeURIComponent(title)}.mdx`),
+export default function Article({ params }: { params: { title: string } }) {
+    const Content = dynamic(()=>import(`../../../news/${decodeURIComponent(params.title)}.mdx`),
     {
         loading: ()=><p>Loading...</p>,
         ssr: false
     }
     )
-    return {
-        props: { Content }
-    }
-}
-
-export default function Article({ Content }: { Content: ComponentType }) {
-    // const Content = dynamic(()=>import(`../../../news/${decodeURIComponent(params.title)}.mdx`),
-    // {
-    //     loading: ()=><p>Loading...</p>,
-    //     ssr: false
-    // }
-    // )
     return (
         <div style={{ gridColumn: "1 / 13", display: "flex", flexDirection: "column", gap: "10px", paddingBottom: "60px", paddingRight: "30px" }}>
             <Suspense fallback={<></>}>
