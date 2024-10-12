4b501171cbd1e04ed6f755801afd9561e134033b
ArtemPodloboshnikov <ArtemPS777@yandex.ru>
2024-10-12 11:42:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: "РСКА",
   description: "РСКА — Российское Сотружество Касима-Син-рю и Айкидо",
+  openGraph: {
+    images: ["/ruka/logo.svg"],
+    title: "РСКА",
+    description: "РСКА — Российское Сотружество Касима-Син-рю и Айкидо"
+  }
 };
 
 export default function RootLayout({
