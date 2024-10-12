697adb2d0e383d8c79ba4af0dcdd0fbbf3a6fd74
ArtemPodloboshnikov <ArtemPS777@yandex.ru>
2024-10-12 23:01:01 +0300

    feat(map icon)

--- app/dojo/[title]/Wrap.tsx
+++ app/dojo/[title]/Wrap.tsx
@@ -1,12 +1,19 @@
 "use client"
 
 import { dojo, domainIcon } from "@/constants/system"
-import { Globe } from "lucide-react"
+import { Globe, MapPin } from "lucide-react"
 import dynamic from "next/dynamic"
 import Image from "next/image"
 import Link from "next/link"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import L from 'leaflet'
 
+
+const NewMarker = new L.Icon({
+    iconUrl: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4iPjxwYXRoIGQ9Ik0yMCAxMGMwIDQuOTkzLTUuNTM5IDEwLjE5My03LjM5OSAxMS43OTlhMSAxIDAgMCAxLTEuMjAyIDBDOS41MzkgMjAuMTkzIDQgMTQuOTkzIDQgMTBhOCA4IDAgMCAxIDE2IDAiLz48Y2lyY2xlIGN4PSIxMiIgY3k9IjEwIiByPSIzIi8+PC9zdmc+",
+    iconSize: [25, 35],
+    iconAnchor: [5, 30]
+})
 
 export default function Wrap({ title }: { title: string }) {
     const info = dojo.find(d=>d.title === title)!
@@ -50,12 +57,12 @@ export default function Wrap({ title }: { title: string }) {
                 )}
             </div>
             {info.address.map((addr, idx)=>(
-                <MapContainer key={idx} center={info.coords[idx]} zoom={13} style={{ height: "500px" }}>
+                <MapContainer key={idx} center={{ lat: info.coords[idx][0], lng: info.coords[idx][1] }} zoom={13} style={{ height: "500px", outline: "none" }}>
                             <TileLayer
                             attribution=''
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             />
-                    <Marker position={info.coords[idx]}>
+                    <Marker position={{ lat: info.coords[idx][0], lng: info.coords[idx][1] }} icon={NewMarker}>
                         <Popup>{addr}</Popup>
                     </Marker>
                 </MapContainer>
